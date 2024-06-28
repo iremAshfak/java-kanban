faa0bfb19d3ee96aa77df11b9ef9e417e875cd28
ashhirka <66332800+ashirka@users.noreply.github.com>
2024-06-28 18:35:05 +0300

    new realisation of history manager and tests for sprint 6

--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -1,34 +1,13 @@
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryHistoryManagerTest {
 
-    HistoryManager historyManager = Managers.getDefaultHistory();
-    TaskManager taskManager = Managers.getDefault();
+    private final HistoryManager historyManager = Managers.getDefaultHistory();
+    private final TaskManager taskManager = Managers.getDefault();
 
-    @Test
-    void addTaskInHistory() {
-        Task task = new Task("Test addTaskInHistory", "Test addTaskInHistory description", Status.NEW);
-        historyManager.add(task);
-        final ArrayList<Task> history = historyManager.getHistory();
-        assertNotNull(history, "История не пустая.");
-        assertEquals(1, history.size(), "История не пустая.");
-    }
-
-    @Test
-    void historySizeShouldBeUnlimited() {
-        for (int i = 0; i < 1000; i++) {
-            Task task = new Task("Test historySizeShouldBeUnlimited", "Test historySizeShouldBeUnlimited description", Status.NEW);
-            taskManager.createTask(task);
-            taskManager.getTask(task.getId());
-        }
-        final ArrayList<Task> history = historyManager.getHistory();
-        assertNotNull(history, "История не пустая.");
-        assertEquals(1000, history.size(), "Размер истории все еще ограничен");
-    }
 
     @Test
     void noRepeatsInHistory() {
@@ -37,7 +16,7 @@ class InMemoryHistoryManagerTest {
         taskManager.getTask(task.getId());
         taskManager.getTask(task.getId()); //просматриваем одну и ту же задачу 2 раза и проверяем историю
 
-        final ArrayList<Task> history = historyManager.getHistory();
+        ArrayList<Task> history = historyManager.getHistory();
         assertEquals(1, history.size(), "Предыдущий просмотр одной и той же задачи не удаляется");
     }
 
@@ -48,7 +27,21 @@ class InMemoryHistoryManagerTest {
         taskManager.getTask(task.getId());
         taskManager.deleteTask(task.getId()); // удаляем просмотренную задачу и проверяем историю
 
-        final ArrayList<Task> history = historyManager.getHistory();
+        ArrayList<Task> history = historyManager.getHistory();
         assertEquals(0, history.size(), "После удаления задачи она не удалилась из истории");
     }
+
+
+    @Test
+    void historySizeShouldBeUnlimited() {
+        for (int i = 0; i < 100; i++) {
+            Task task = new Task("Test historySizeShouldBeUnlimited", "Test historySizeShouldBeUnlimited description", Status.NEW);
+            taskManager.createTask(task);
+            taskManager.getTask(task.getId());
+        }
+        ArrayList<Task> history = historyManager.getHistory();
+        assertNotNull(history, "История не пустая.");
+        assertEquals(101, history.size(), "Размер истории все еще ограничен");
+    }
+
 }
